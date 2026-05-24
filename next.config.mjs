637bc6cfcf47b2d4ce5c34ef965c1d83ec21acf1
sami-ux-ui <mainalisamir57@gmail.com>
2026-05-24 23:19:50 +0545

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  typescript: {
+    // Dangerously allow production builds to successfully complete even if
+    // your project has TypeScript errors.
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
